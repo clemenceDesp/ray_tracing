44760d1683cd42211b1f4d77730038b7404b5d5a
Silvert Felix <felix.silvert.04@gmail.com>
2023-10-23 13:15:50 +0200

    Représentation des différentes lumières

---
+++ src/main/fr/univartois/iutl/info/raytracing/parser/DirectionalLight.java
@@ -0,0 +1,49 @@
+package fr.univartois.iutl.info.raytracing.parser;
+
+import java.awt.Color;
+import java.awt.Point;
+
+/**
+ * @author felix
+ *
+ */
+public class DirectionalLight implements Light{
+
+	/**
+	 * the coordinates of the light
+	 */
+	private Point coordinates; 
+	
+	/**
+	 * the colors of the light
+	 */
+	private Color colors; 
+	
+	/**
+	 * return the coordinates
+	 */
+	public Point getCoordinates() {
+		return this.coordinates;
+	}
+
+	/**
+	 * set the coordinates
+	 */
+	public void setCoordinates(Point coordinates) {
+		this.coordinates=coordinates;
+	}
+
+	/**
+	 * return the colors
+	 */
+	public Color getColors() {
+		return this.colors;
+	}
+
+	/**
+	 * set the colors
+	 */
+	public void setColors(Color colors) {
+		this.colors=colors;
+	}
+}
