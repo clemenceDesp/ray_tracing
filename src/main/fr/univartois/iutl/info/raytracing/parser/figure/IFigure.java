f7c9fbf99b78149e0c8d75f8b2fc7fb333667f1a
Roméo Jacquemin <romeo_jacquemin@ens.univ-artois.fr>
2023-10-24 14:48:07 +0200

    correction de l'interface des figures et de l'implémentation de la méthode

--- src/main/fr/univartois/iutl/info/raytracing/parser/figure/IFigure.java
+++ src/main/fr/univartois/iutl/info/raytracing/parser/figure/IFigure.java
@@ -1,6 +1,7 @@
 package fr.univartois.iutl.info.raytracing.parser.figure;
 
 import fr.univartois.iutl.info.raytracing.numeric.Point;
+import fr.univartois.iutl.info.raytracing.numeric.Vector;
 
 /**
  * The {@link IFigure} interface defines the contract to be fulfilled
@@ -13,4 +14,6 @@ public interface IFigure {
      * @return The point of origin of this figure.
      */
     Point getOrigin();
+    
+    double findInteraction(Vector d);
 }
