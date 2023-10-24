e2a2d4058ed71adfc15be45376ab78cf9768d371
Clémence Despeghel <clemence_despeghel@ens.univ-artois.fr>
2023-10-24 14:52:08 +0200

    Ajout de méthodes dans l'interface IFigure

--- src/main/fr/univartois/iutl/info/raytracing/parser/figure/IFigure.java
+++ src/main/fr/univartois/iutl/info/raytracing/parser/figure/IFigure.java
@@ -1,6 +1,8 @@
 package fr.univartois.iutl.info.raytracing.parser.figure;
 
+import fr.univartois.iutl.info.raytracing.numeric.Color;
 import fr.univartois.iutl.info.raytracing.numeric.Point;
+import fr.univartois.iutl.info.raytracing.parser.Light;
 
 /**
  * The {@link IFigure} interface defines the contract to be fulfilled
@@ -13,4 +15,25 @@ public interface IFigure {
      * @return The point of origin of this figure.
      */
     Point getOrigin();
+
+    /**
+     * Gives the diffuse color of the object.
+     *
+     * @return The diffuse color of the object.
+     */
+    Color getDiffuse();
+
+    /**
+     * Gives the reflected light.
+     *
+     * @return The reflected light.
+     */
+    Light getSpecular();
+
+    /**
+     * Gives the shininess of the object.
+     *
+     * @return The shininess of the object.
+     */
+    int getShininess();
 }
