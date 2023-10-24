bf0eff20b520148764002b698a5da73a4446b845
Clémence Despeghel <clemence_despeghel@ens.univ-artois.fr>
2023-10-24 10:11:00 +0200

    Ajout de la méthode point dans Parser

--- src/main/fr/univartois/iutl/info/raytracing/parser/Parser.java
+++ src/main/fr/univartois/iutl/info/raytracing/parser/Parser.java
@@ -72,6 +72,22 @@ public class Parser {
         sceneBuilder.addLight(new DirectionalLight(directional, color));
     }
 
+    /**
+     * Configures a point light.
+     * @param line Line that is being read.
+     */
+    private static void point(String[] line) {
+        Point point = new Point(new Triplets(new Coordinates(
+                Double.parseDouble(line[1]),
+                Double.parseDouble(line[2]),
+                Double.parseDouble(line[3]))));
+        Color color = new Color(new Triplets(new Coordinates(
+                Double.parseDouble(line[4]),
+                Double.parseDouble(line[5]),
+                Double.parseDouble(line[6]))));
+        sceneBuilder.addLight(new PunctualLight(point, color));
+    }
+
     public static Scene read(String fileName) {
         BufferedReader bufferedreader = null;
         FileReader filereader = null;
@@ -109,15 +125,7 @@ public class Parser {
                         directional(line);
                         break;
                     case "point":
-                        Point point = new Point(new Triplets(new Coordinates(
-                                Double.parseDouble(line[1]),
-                                Double.parseDouble(line[2]),
-                                Double.parseDouble(line[3]))));
-                        color = new Color(new Triplets(new Coordinates(
-                                Double.parseDouble(line[4]),
-                                Double.parseDouble(line[5]),
-                                Double.parseDouble(line[6]))));
-                        sceneBuilder.addLight(new PunctualLight(point, color));
+                        point(line);
                         break;
                     case "maxverts":
                         int nbPoints = Integer.parseInt(line[1]);
