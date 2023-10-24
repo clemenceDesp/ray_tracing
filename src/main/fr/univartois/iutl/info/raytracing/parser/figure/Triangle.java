a621e38c6c2f21693b89eccbb916622dab8cf724
Clémence Despeghel <clemence_despeghel@ens.univ-artois.fr>
2023-10-24 15:30:27 +0200

    Ajout des getters dans la classe Triangle

--- src/main/fr/univartois/iutl/info/raytracing/parser/figure/Triangle.java
+++ src/main/fr/univartois/iutl/info/raytracing/parser/figure/Triangle.java
@@ -85,4 +85,34 @@ public class Triangle implements IFigure {
     public Point getOrigin() {
         return pointA;
     }
+
+    /**
+     * Gives the diffuse color of the figure.
+     *
+     * @return The diffuse color of the figure.
+     */
+    @Override
+    public Color getDiffuse() {
+        return diffuse;
+    }
+
+    /**
+     * Gives the reflected light.
+     *
+     * @return The reflected light.
+     */
+    @Override
+    public Light getSpecular() {
+        return specular;
+    }
+
+    /**
+     * Gives the shininess of the figure.
+     *
+     * @return The shininess of the figure.
+     */
+    @Override
+    public int getShininess() {
+        return shininess;
+    }
 }
