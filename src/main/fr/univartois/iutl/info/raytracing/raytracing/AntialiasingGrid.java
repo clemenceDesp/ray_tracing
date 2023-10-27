2ad25ed9bff475c29e7a17077c6d6704c75ed184
Humez Matteo <matteo_humez@ens.univ-artois.fr>
2023-10-27 15:47:47 +0200

    :bug: correction anti crénelage grid quand nb est impair

--- src/main/fr/univartois/iutl/info/raytracing/raytracing/AntialiasingGrid.java
+++ src/main/fr/univartois/iutl/info/raytracing/raytracing/AntialiasingGrid.java
@@ -27,20 +27,21 @@ public class AntialiasingGrid extends AbstractRayTracing{
         int b = 0;
         int numberSampling = getScene().getNumberSampling();
         int numberSamplingDiv = numberSampling/2;
+        int nb = 0;
         for (int i = -numberSamplingDiv; i < numberSamplingDiv; i++) {
             for (int j = -numberSamplingDiv; j < numberSamplingDiv; j++) {
                 if (x + i < 0 || x + i >= image.getWidth() || y + j < 0 || y + j >= image.getHeight()) {
                     continue;
                 }
+                nb++;
                 int color = image.getRGB(x + i, y + j);
                 r += (color>>16)&0xFF;
                 g += (color>>8)&0xFF;
                 b += (color)&0xFF;
             }
         }
-        if (numberSampling > 0) {
-            int numberSamplingSquare = numberSampling * numberSampling;
-            return new Color(r / numberSamplingSquare, g / numberSamplingSquare, b / numberSamplingSquare);
+        if (numberSampling > 0 && nb != 0) {
+            return new Color(r / nb, g / nb, b / nb);
         } else {
             return new Color(r, g, b);
         }
